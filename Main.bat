f8527d10b30c6faacb984be736ab6d6d3faeba53
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-08 15:17:18 -0500

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,2 +1,22 @@
+::UpdateMain
+::Version 1.0
+FIND "Version 1.0" C:\apps\main.bat
+IF %ERRORLEVEL%==0 GOTO Updated
 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat C:\Apps\Main.bat
-MD C:\Scripts
+C:\apps\main.bat
+exit
+:Updated
+::CleanupVMwareClientDumpFiles
+RD C:\ProgramData\VMware\VDM /S /Q
+RD "C:\Users\United Way\AppData\Local\VMware\VDM" /S /Q
+RD "C:\Users\CFSC\AppData\Local\VMware\VDM" /S /Q
+
+::Filters
+
+::Check VMware Client Version
+reg query "HKLM\SOFTWARE\WOW6432Node\VMware, Inc.\VMware VDM\Client" /d /f "8.3.0"
+IF %errorlevel%==0 EXIT
+reg query "HKLM\SOFTWARE\WOW6432Node\VMware, Inc.\VMware VDM\Client" /d /f "5.5.2"
+IF %errorlevel%==0 EXIT
+
+ECHO Done > C:\test.txt
