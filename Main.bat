129fd2f39275c2a0548836d5dfbc63f4eb6ae0f5
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-08 16:25:58 -0500

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -2,13 +2,20 @@
 IF NOT EXIST C:\Apps MD C:\Apps
 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat C:\Apps\Main.bat
 
+ECHO Main Updated >> C:\log.txt
+
 ::CleanupVMwareClientDumpFiles
 RD C:\ProgramData\VMware\VDM /S /Q
 RD "C:\Users\United Way\AppData\Local\VMware\VDM" /S /Q
 RD "C:\Users\CFSC\AppData\Local\VMware\VDM" /S /Q
 
+ECHO DumpClean >> C:\log.txt
+
 ::Filters
 
 ::Scripts
 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/VMwareUpdate.bat C:\Apps\UpdateVMware.bat
+
+ECHO CallingClientUpdate >> C:\log.txt
+
 CALL C:\Apps\VMwareUpdate.bat
