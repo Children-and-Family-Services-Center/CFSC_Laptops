46d15a47bd3aae8cd178650d3b03d8f8d1668a17
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-08 16:08:27 -0500

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,5 +1,5 @@
 ::UpdateMain
-IF NOEXIST C:\Apps MD C:\Apps
+IF NOT EXIST C:\Apps MD C:\Apps
 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat C:\Apps\Main.bat
 
 ::CleanupVMwareClientDumpFiles
