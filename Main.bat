9677d6635f0ecb65d4567400afaa596904685d4b
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-09 12:11:03 -0500

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,9 +1,9 @@
-SET Version=Version 1.2
+SET Version=Version 1.3
 ::UpdateMain
 IF NOT EXIST C:\Apps MD C:\Apps
 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat C:\Apps\Main.bat
 FIND "%Version%" C:\Apps\Main.bat
-IF %ERRORLEVEL%==1 SCHTASKS /RUN /TN "CFSC_Main" & EXIT
+IF %ERRORLEVEL%==1 SCHTASKS /RUN /TN "CFSC_Main" & ECHO Restart >> C:\log.txt & EXIT
 ECHO Main Updated >> C:\log.txt
 
 ::CleanupVMwareClientDumpFiles
