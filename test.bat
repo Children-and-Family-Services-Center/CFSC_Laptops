75dfa73420f56d44c3240b05adbe7d36feec79a7
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-11 13:10:35 -0500

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,42 @@
+SET Version=Version 3.4
+IF NOT EXIST C:\Apps MD C:\Apps
+ECHO. >> C:\Apps\log.txt
+ECHO %date% %time% >> C:\Apps\log.txt
+ECHO %Version% >> C:\Apps\log.txt
+ECHO Start - %time% >> C:\Apps\log.txt
+
+CALL :CheckInternet
+CALL :UpdateMain
+
+ECHO Finish - %time% >> C:\Apps\log.txt
+EXIT
+
+::CheckInternet--------------------------------------------------------------------
+:CheckInternet
+ECHO CheckInternet - %time% - Start >> C:\Apps\log.txt
+SET REPEAT=0
+:REPEAT
+IF %REPEAT%==5 ECHO CheckInternet - No Internet >> C:\Apps\log.txt & EXIT
+SET /a REPEAT=%REPEAT%+1
+ECHO CheckInternet - Attempt %REPEAT% %time% >> C:\Apps\log.txt
+PING google.com -n 1
+IF %ERRORLEVEL%==1 TIMEOUT /T 20 & GOTO REPEAT
+ECHO CheckInternet - %time% - Finish >> C:\Apps\log.txt
+EXIT /b
+
+
+::UpdateMain-----------------------------------------------------------------------
+:UpdateMain
+IF %PROCESSOR_ARCHITECTURE%==AMD64 Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat -O C:\Apps\Main.bat
+IF %PROCESSOR_ARCHITECTURE%==x86 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat C:\Apps\Main.bat
+FIND "%Version%" C:\Apps\Main.bat
+IF %ERRORLEVEL%==0 ECHO UpdateMain - Updated >> C:\Apps\log.txt & EXIT /b
+ECHO UpdateMain - OutDated - Updating >> C:\Apps\log.txt
+ECHO SLEEP 10 > %temp%\temp.bat
+ECHO C:\apps\Main.bat >> %temp%\temp.bat
+ECHO UpdateMain - OutDated - Relaunching >> C:\Apps\log.txt
+%temp%\temp.bat
+EXIT
+
+
+::---------------------------------------------------------------------------------
