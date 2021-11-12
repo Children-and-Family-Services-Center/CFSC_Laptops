d3dac6d39d97b5e8a065ed13897afba8e24ddfec
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-12 14:54:05 -0500

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,4 +1,4 @@
-SET Version=Version 3.26
+SET Version=Version 3.27
 IF NOT EXIST C:\Apps MD C:\Apps
 ECHO. >> C:\Apps\log.txt
 ECHO %date% %time% >> C:\Apps\log.txt
@@ -51,7 +51,7 @@ EXIT /b
 ::UpdateVMMwareClient---------------------------------------------------------------
 :UpdateVMwareClient
 ECHO %time% - UpdateVMwareClient - Start >> C:\Apps\log.txt
-REG ADD "HKLM\SOFTWARE\VMware, Inc.\VMware VDM\Client" /T REG_SZ /V ServerURL /D view.childrenfamily.org /f
+REG ADD "HKLM\SOFTWARE\WOW6432Node\VMware, Inc.\VMware VDM\Client" /T REG_SZ /V ServerURL /D view.childrenfamily.org /f
 IF %PROCESSOR_ARCHITECTURE%==x86 GOTO OLD
 :NEW
 reg query "HKLM\SOFTWARE\WOW6432Node\VMware, Inc.\VMware VDM\Client" /d /f "8.3.0"
