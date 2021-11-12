a209178efed864c1cc131808802b5e4c065e7e90
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-12 12:50:20 -0500

    Update FirstRun.bat

--- FirstRun.bat
+++ FirstRun.bat
@@ -6,9 +6,11 @@ for /f "usebackq delims=" %%p in (`%psCommand%`) do set password=%%p
 CLS
 NET USER Administrator /ACTIVE:YES
 NET USER Administrator %password%
+IF NOT %USERNAME%==CFSC NET USER CFSC /Add
+IF %USERNAME%==CFSC NET LOCALGROUP Administrators CFSC /DELETE
 IF NOT EXIST C:\Apps MD C:\Apps
 SCHTASKS /query /TN CFSC_Main
 IF %ERRORLEVEL%==1 SCHTASKS /CREATE /SC ONSTART /TN "CFSC_Main" /TR "C:\Apps\Main.bat" /RU SYSTEM /NP /V1 /F
 IF %PROCESSOR_ARCHITECTURE%==AMD64 Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat -O C:\Apps\Main.bat
 IF %PROCESSOR_ARCHITECTURE%==x86 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat C:\Apps\Main.bat
-CALL C:\Apps\Main.bat
+shutdown /r /t 10
