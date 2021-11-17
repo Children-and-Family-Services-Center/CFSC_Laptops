90da3ed103f3865dbbaa715cb794f6a6161f13bf
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-17 16:39:11 -0500

    Update FirstRun.bat

--- FirstRun.bat
+++ FirstRun.bat
@@ -1,4 +1,3 @@
-ECHO OFF
 set "psCommand=powershell -Command "$pword = read-host 'Administrator Password? ' -AsSecureString ; ^
     $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
         [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
@@ -9,14 +8,15 @@ CALL :RenamePC
 CALL :SetupUserAccounts
 CALL :InstallApps
 CALL :ActivateMainScript
-C:\apps\apps.bat
+CLS
+ECHO Restarting PC, Login as Administrator...
+PAUSE
+SHUTDOWN -r -t 0
 EXIT
 
 ::ActivateMainScript-----------------------------------------------------
 :ActivateMainScript
 IF NOT EXIST C:\Apps MD C:\Apps
-SCHTASKS /query /TN CFSC_Main
-IF %ERRORLEVEL%==1 SCHTASKS /CREATE /SC ONSTART /TN "CFSC_Main" /TR "C:\Apps\Main.bat" /RU SYSTEM /NP /V1 /F
 IF %PROCESSOR_ARCHITECTURE%==AMD64 Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat -O C:\Apps\Main.bat
 IF %PROCESSOR_ARCHITECTURE%==x86 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat C:\Apps\Main.bat
 EXIT /b
@@ -34,7 +34,8 @@ EXIT /b
 :SetupUserAccounts
 NET USER Administrator /ACTIVE:YES
 NET USER Administrator %password%
-for /F %i in ('net localgroup group_name') do net localgroup group_name %i /delete
+for /F %%i in ('net localgroup Administrators') do net localgroup Administrators %%i /delete
+NET USER CFSC /ADD
 NET LOCALGROUP Users CFSC /ADD
 WMIC UserAccount WHERE "Name='CFSC'" SET PasswordExpires=FALSE
 WMIC UserAccount WHERE "Name='CFSC'" SET PasswordChangeable=FALSE
@@ -43,9 +44,16 @@ EXIT /b
 ::InstallApps-----------------------------------------------------
 :InstallApps
 POWERSHELL Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-ECHO choco install adobereader -y >> C:\apps\Apps.bat
-ECHO choco install googlechrome -y >> C:\apps\Apps.bat
-ECHO choco install firefox -y >> C:\apps\Apps.bat
-ECHO choco install vlc -y >> C:\apps\Apps.bat
-ECHO DEL C:\Apps\Apps.bat /f /q
+IF NOT EXIST "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" MD "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+ECHO choco install adobereader -y >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+ECHO choco install googlechrome -y >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup.bat"
+ECHO choco install firefox -y >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+ECHO choco install vlc -y >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+ECHO choco install vmware-horizon-client -y >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+ECHO DEL "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat" /f /q  >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+ECHO DEL "C:\Users\Public\Desktop\Firefox.lnk" /f /q  >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+ECHO DEL "C:\Users\Public\Desktop\VLC media player.lnk" /f /q  >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+ECHO SCHTASKS /query /TN CFSC_Main  >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+ECHO IF %ERRORLEVEL%==1 SCHTASKS /CREATE /SC ONSTART /TN "CFSC_Main" /TR "C:\Apps\Main.bat" /RU SYSTEM /NP /V1 /F  >> "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Apps.bat"
+
 EXIT /b
