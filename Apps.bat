c9e294bec276838131ee70a44ec3bd96a6d2bd3a
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-11-22 12:09:48 -0500

    Update Apps.bat

--- Apps.bat
+++ Apps.bat
@@ -1,8 +1,8 @@
-choco install adobereader -y
-choco install googlechrome -y
-choco install firefox -y
-choco install vlc -y
-choco install vmware-horizon-client -y
+choco upgrade adobereader -y --install-if-not-installed
+choco upgrade googlechrome -y --install-if-not-installed
+choco upgrade firefox -y --install-if-not-installed
+choco upgrade vlc -y --install-if-not-installed
+choco upgrade vmware-horizon-client -y --install-if-not-installed
 DEL "C:\Users\Public\Desktop\Firefox.lnk" /f /q
 DEL "C:\Users\Public\Desktop\VLC media player.lnk" /f /q
 SCHTASKS /CREATE /SC ONSTART /TN "CFSC_Main" /TR "C:\Apps\Main.bat" /RU SYSTEM /NP /V1 /F
