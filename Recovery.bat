642f19aa65dca07f6ec9bc6f7f1838673f42750f
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2021-12-20 15:18:13 -0500

    Update Recovery.bat

--- Recovery.bat
+++ Recovery.bat
@@ -1,3 +1,3 @@
-IF NOT EXISTS MD C:\Recovery\AutoApply
+MD C:\Recovery\AutoApply
 Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/unattend.xml -O C:\Recovery\AutoApply\unattend.xml
 Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/WiFi-CFSCPublicPW.xml -O C:\Recovery\WiFi-CFSCPublicPW.xml
