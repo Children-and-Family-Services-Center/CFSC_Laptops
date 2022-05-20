ae5af1b34c001b5cb9ba81c4aa29960b068a69ec
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2022-05-20 14:05:46 -0400

    Update

--- Apps.bat
+++
@@ -1,9 +0,0 @@
-choco upgrade adobereader -y --install-if-not-installed
-choco upgrade googlechrome -y --install-if-not-installed
-choco upgrade firefox -y --install-if-not-installed
-choco upgrade vlc -y --install-if-not-installed
-choco upgrade vmware-horizon-client -y --install-if-not-installed
-DEL "C:\Users\Public\Desktop\Firefox.lnk" /f /q
-DEL "C:\Users\Public\Desktop\VLC media player.lnk" /f /q
-SCHTASKS /CREATE /SC ONSTART /TN "CFSC_Main" /TR "C:\Apps\Main.bat" /RU SYSTEM /NP /V1 /F
-SCHTASKS /RUN /TN "CFSC_Main"
