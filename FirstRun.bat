fd4435cf938edf4fdac7a687378eac12177d9804
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2022-06-08 11:48:52 -0400

    Update FirstRun.bat

--- FirstRun.bat
+++ FirstRun.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET Version=Version 2.8
+SET Version=Version 2.9
 IF NOT EXIST C:\Apps MD C:\Apps
 ECHO. >> C:\Apps\log.txt
 ECHO %date% %time% >> C:\Apps\log.txt
@@ -19,6 +19,7 @@ CALL :SetupUserAccounts
 CALL :InstallChoco
 CALL :ActivateMainScript
 CALL :AutoLogon
+CALL :Recovery
 
 CLS
 IF EXIST C:\Recovery\AutoApply\Test GOTO test
@@ -115,4 +116,16 @@ SCHTASKS /CREATE /SC ONSTART /TN "CFSC_Main" /TR "C:\Apps\Main.bat" /RU SYSTEM /
 IF %PROCESSOR_ARCHITECTURE%==AMD64 Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat -O C:\Apps\Main.bat
 IF %PROCESSOR_ARCHITECTURE%==x86 bitsadmin /transfer VMware /download /priority normal https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Main.bat C:\Apps\Main.bat
 ECHO %time% - ActivateMainScript - Finished >> C:\Apps\log.txt
+EXIT /b
+
+::Recovery--------------------------------------------------------------------
+:Recovery
+ECHO %time% - Recovery Started >> C:\Apps\log.txt
+ICACLS C:\Recovery /setowner SYSTEM /T /C /Q
+ICACLS C:\Recovery /reset /T /C /Q
+RD C:\Recovery /s /q & MD C:\Recovery & MD C:\Recovery\AutoApply
+Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/unattend.xml -O C:\Recovery\AutoApply\unattend.xml
+Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/WiFi-CFSCPublicPW.xml -O C:\Recovery\AutoApply\WiFi-CFSCPublicPW.xml
+Powershell Invoke-WebRequest https://raw.githubusercontent.com/Children-and-Family-Services-Center/CFSC_Laptops/main/Restore.bat -O C:\Recovery\AutoApply\Restore.bat
+ECHO %time% - Recovery Finished >> C:\Apps\log.txt
 EXIT /b
